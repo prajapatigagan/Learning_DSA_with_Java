da9cabadedad827a84b5c91e9a2163c1cede374c
Gagan Prajapati <gaganprajapati121@gmail.com>
2026-03-28 23:16:00 +0530

    initial commit

---
+++ BINARY_SEARCH/SearchNumber.java
@@ -0,0 +1,30 @@
+package BINARY_SEARCH;
+
+public class SearchNumber {
+
+    public static int Search(int numbers[], int key){
+       int low=0;
+       int high=numbers.length-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(numbers[mid]==key){
+            return mid;
+            }
+            else if(numbers[mid]<key){
+                low=mid+1;
+            }
+            else{
+
+                high=mid-1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int numbers[]={2,3,4,5,6,7,8,9};
+        int key=5;
+        int ans=Search(numbers,key);
+        System.out.println("target is at index: "+ans);
+    }
+    
+}
