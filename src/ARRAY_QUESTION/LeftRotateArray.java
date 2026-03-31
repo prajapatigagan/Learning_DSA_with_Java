95bff46cb563c9b3a910ddec7273bad72c918fe1
Gagan Prajapati <gaganprajapati121@gmail.com>
2026-03-31 23:55:54 +0530

    initial commit

---
+++ src/ARRAY_QUESTION/LeftRotateArray.java
@@ -0,0 +1,22 @@
+package ARRAY_QUESTION;
+
+public class LeftRotateArray {
+    public static void leftRotate(int num[],int d){
+        
+        for(int k=0; k<d; k++){
+            int first=num[0];
+            for(int i=0; i<num.length-1; i++){
+                num[i]=num[(i+1)];
+            }
+            num[num.length-1]=first;
+        }
+    }
+    public static void main(String[] args) {
+        int num[]={1,2,3,4,5};
+        int d=3;
+        leftRotate(num,d);
+        for(int i=0; i<num.length; i++){
+            System.out.print(num[i]+" ");
+        }
+    }   
+}
---
+++ src/BINARY_SEARCH/MinimuminRotatedSortedArray.java
@@ -0,0 +1,8 @@
+package BINARY_SEARCH;
+
+public class MinimuminRotatedSortedArray {
+    public static int rotatedSortedArray(int num[], int key){
+    
+        return 0;
+    }
+}
