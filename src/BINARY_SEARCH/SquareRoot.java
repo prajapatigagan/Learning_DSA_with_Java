975c944c8b3454bae42919d835ef5fb5d905158c
Gagan Prajapati <gaganprajapati121@gmail.com>
2026-04-03 22:01:58 +0530

    initial commit

---
+++ src/BINARY_SEARCH/SquareRoot.java
@@ -0,0 +1,27 @@
+package BINARY_SEARCH;
+
+public class SquareRoot {
+    public static int Square(int n){
+        int start=0; int end=n;
+        int ans=-1;
+        while(start<end){
+            int mid=start+(end-start)/2;
+
+            if(mid*mid==n){
+                return mid;
+            }
+            if(mid*mid<n){
+                ans=mid;
+                start=mid+1;
+            } else {
+                end=mid;
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int n=4;
+        int ans=Square(n);
+        System.out.println("Square root of "+n+" is: "+ans);
+    }
+}
