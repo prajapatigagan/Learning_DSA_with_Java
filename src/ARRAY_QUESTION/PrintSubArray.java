c244ae169efae0f7cfebaaebd4b5a8be69db0582
Gagan Prajapati <gaganprajapati121@gmail.com>
2026-04-05 12:04:48 +0530

    initial commit

---
+++ src/ARRAY_QUESTION/PrintSubArray.java
@@ -0,0 +1,50 @@
+package ARRAY_QUESTION;
+
+public class PrintSubArray {
+    public static void subArray(int arr[]){
+        int ts=0;
+        for(int i=0; i<arr.length; i++){
+            int start=i;
+ 
+            for(int j=i; j<arr.length; j++){
+                int end=j;
+                for(int k=start; k<=end; k++){
+                   
+                    System.out.print(arr[k]+" "); 
+                }
+                System.out.println();  
+                ts++;
+            }
+            System.out.println();
+        }
+        System.out.println("Total subarrays: " + ts);
+    }
+     public static void totalsubArray(int arr[]){
+        int ts=0;
+        for(int i=0; i<arr.length; i++){
+            int start=i;
+ 
+            for(int j=i; j<arr.length; j++){
+                int end=j;
+                for(int k=start; k<=end; k++){
+                   
+                    System.out.print(arr[k]+" "); 
+                    ts+=arr[k];
+                   
+                }
+                 
+                System.out.println();
+                
+            }
+            System.out.print("Sum:"+ts);
+            System.out.println();
+        }
+        System.out.println("Total sum: " + ts);
+    }
+
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5};
+        subArray(arr);
+        totalsubArray(arr);
+    }
+}
--- src/BINARY_SEARCH/ReverseAnArray.java
+++ src/BINARY_SEARCH/ReverseAnArray.java
@@ -11,6 +11,7 @@ public class ReverseAnArray {
             start++;
             last--;
         }
+        
     }
     public static void main(String[] args) {
         int arr[]={1,2,3,4,5,6,7,8,9};
@@ -19,6 +20,7 @@ public class ReverseAnArray {
         for(int i=0; i<arr.length; i++){
             System.out.print(arr[i]+" ");
         }
+        
     }
     
 }
