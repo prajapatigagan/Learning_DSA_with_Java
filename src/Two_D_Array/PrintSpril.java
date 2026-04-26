b14da73a11a9705f067365968940513e34542467
Gagan Prajapati <gaganprajapati121@gmail.com>
2026-04-27 00:08:35 +0530

    initial commit

--- src/Two_D_Array/PrintSpril.java
+++ src/Two_D_Array/PrintSpril.java
@@ -12,32 +12,39 @@ public class PrintSpril {
 
             //top
             for(int j=startcol; j<=endcol; j++){
-                System.out.println(matrix[startrow][j]+" ");
+                System.out.print(matrix[startrow][j]+" ");
             }
             //right
             for(int i=startrow+1; i<=endrow; i++){
-                System.out.println(matrix[i][endcol]+" ");
+                System.out.print(matrix[i][endcol]+" ");
             }
-
             //bottom
-            for(int j=endcol-1; j>startcol;j--){
-                System.out.println(matrix[endrow][j]+" ");
+            for(int j=endcol-1; j>startcol+1;j--){
+                if(startrow==endrow){
+                    break;
+                }
+                System.out.print(matrix[endrow][j]+" ");
             } 
             //left
-            for(int i=endrow-1; i>=startrow+1; i--){
-                System.out.println(matrix[i][startcol]+" ");
+            for(int i=endrow; i>=startrow+1; i--){
+                if(startcol==endcol){
+                    break;
+                }
+                System.out.print(matrix[i][startcol]+" ");
             }
             startcol++;
             endcol--;
             startrow++;
             endrow--;
         }
+        System.out.println();
     }
     public static void main(String[] args) {
-        int array[][]={{1,2,3,4},
+        int matrix[][]={{1,2,3,4},
                        {5,6,7,8},
                        {9,10,11,12},
                     {13,14,15,16}};
-    
+
+                    printspiral(matrix);
 }
 }
