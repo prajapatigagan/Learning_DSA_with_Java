4ac3caf379efe92a219cd059a905a14bb7be014e
Gagan Prajapati <gaganprajapati121@gmail.com>
2026-04-28 23:51:20 +0530

    initial commit

---
+++ src/String/Basic.java
@@ -0,0 +1,17 @@
+package String;
+
+import java.util.Scanner;
+
+public class Basic {
+    public static void main(String[] args) {
+        char arr[]={'a','b','c','d','e'};
+        String str="abcd";
+        String str2=new String("xyz");
+        
+        //String are IMMUTABLE
+        Scanner sc =new Scanner(System.in);
+        String name;
+        name=sc.nextLine();
+        System.out.println("name");
+    }
+}
