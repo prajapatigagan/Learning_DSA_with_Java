a4379298766f69b8f097906ee5fa41ad771225e2
Gagan Prajapati <gaganprajapati121@gmail.com>
2026-04-29 00:00:17 +0530

    initial commit

--- src/String/Basic.java
+++ src/String/Basic.java
@@ -1,17 +1,26 @@
 package String;
 
-import java.util.Scanner;
-
 public class Basic {
     public static void main(String[] args) {
-        char arr[]={'a','b','c','d','e'};
-        String str="abcd";
-        String str2=new String("xyz");
+        // char arr[]={'a','b','c','d','e'};
+        // String str="abcd";
+        // String str2=new String("xyz");
+        // System.out.println(arr[2]);
         
         //String are IMMUTABLE
-        Scanner sc =new Scanner(System.in);
-        String name;
-        name=sc.nextLine();
-        System.out.println("name");
+        // Scanner sc =new Scanner(System.in);
+        // String name;
+        // name=sc.nextLine();
+        // System.out.println("name");
+
+        String fullname="gagandeep";
+        System.out.println(fullname.length());
+
+        //concatination
+
+        String firstname="Gagan";
+        String lastname="Prajapati";
+        String fullnamee=firstname +" "+ lastname;
+        System.out.println(fullnamee);
     }
 }
