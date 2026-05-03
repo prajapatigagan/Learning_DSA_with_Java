17a97a3c8449664a701116f622333c35d2dafedb
Gagan Prajapati <gaganprajapati121@gmail.com>
2026-05-03 23:37:02 +0530

    initial commit

---
+++ src/BitManipulation/Basic.java
@@ -0,0 +1,14 @@
+package BitManipulation;
+
+public class Basic {
+
+    public static void main(String[] args) {
+        //binary AND
+        System.out.println(5&6);
+        //binary OR
+        System.out.println(8|9);
+        //binary XOR
+        System.out.println(5^6);
+
+    }
+}
