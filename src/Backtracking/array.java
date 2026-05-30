79e74d52b79d267322602446ce79d7e6cba44892
Gagan Prajapati <gaganprajapati121@gmail.com>
2026-05-31 00:49:40 +0530

    initial commit

---
+++ src/Backtracking/array.java
@@ -0,0 +1,26 @@
+package Backtracking;
+
+public class array {
+    public static void changearr(int arr[],int i,int val){
+        if(i==arr.length){
+            printarr(arr);
+            return;
+        }
+         arr[i]=val;
+         changearr(arr, i+1, val+1);
+         arr[i]=arr[i]-2;
+    }
+    public static void printarr(int arr[]){
+        int n=arr.length;
+        for(int i=0; i<n; i++){
+            System.out.print(arr[i]);
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr[]=new int[5];
+        changearr(arr,0,1);
+        printarr(arr);
+    }
+    
+}
