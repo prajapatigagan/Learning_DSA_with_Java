dc80db40f6fcbe3c54f4c86a1dec50bbab43a589
Gagan Prajapati <gaganprajapati121@gmail.com>
2026-07-06 23:05:24 +0530

    Initial commit

---
+++ src/ArrayList/MultidimensionalArraylist.java
@@ -0,0 +1,76 @@
+package ArrayList;
+
+import java.util.ArrayList;
+
+public class MultidimensionalArraylist {
+    // public static void main(String[] args) {
+    //     ArrayList<ArrayList<Integer>> mainlist=new ArrayList<>();
+
+    //     ArrayList<Integer> list1=new ArrayList<>();
+
+    //     ArrayList<Integer> list2=new ArrayList<>();
+        
+
+
+    //     list1.add(1);
+    //     list1.add(2);
+    //     list1.add(3);
+    //     list1.add(4);
+    //     list1.add(5);
+
+    //     mainlist.add(list1);
+
+      
+    //     list2.add(2);
+    //     list2.add(4);
+    //     list2.add(6);
+    //     list2.add(8);
+    //     list2.add(10);
+
+    //     mainlist.add(list2);
+
+
+    //     System.out.println(mainlist);
+
+    //     for(int i=0; i<mainlist.size(); i++){
+    //         ArrayList<Integer> currlist=mainlist.get(i);
+    //         for(int j=0;j<currlist.size();j++){
+    //             System.out.print(currlist.get(j)+" ");
+    //         }
+    //         System.out.println();
+    //     }
+
+    // }
+
+
+
+    //add two arraylist and print three..
+   public static void main(String[] args) {
+    ArrayList<ArrayList<Integer>> mainlist=new ArrayList<>();
+    ArrayList<Integer> list1=new ArrayList<>();
+    ArrayList<Integer> list2=new ArrayList<>();
+    ArrayList<Integer> list3=new ArrayList<>();
+
+    for(int i=1;i<=5;i++){
+        list1.add(i*1);
+        list2.add(i*2);
+        list3.add(i*3);
+
+    }
+
+    mainlist.add(list1);
+    mainlist.add(list2);
+    mainlist.add(list3);
+
+    System.out.println(mainlist);
+
+    for(int i=0; i<mainlist.size();i++){
+        ArrayList<Integer> currlist=mainlist.get(i);
+        for(int j=0;j<currlist.size();j++){
+            System.out.print(currlist.get(j)+" ");
+        }
+        System.out.println();
+    }
+
+   }
+}
