5fe62adab949da66dc1d1c8be4efe3cce5a28c25
Gagan Prajapati <gaganprajapati121@gmail.com>
2026-07-07 00:18:31 +0530

    Initial commit

---
+++ src/ArrayList/ContainMostWater.java
@@ -0,0 +1,36 @@
+package ArrayList;
+
+import java.util.ArrayList;
+
+import TwoPointer.sortedSquares;
+
+public class ContainMostWater {
+    public static int storewater(ArrayList<Integer> height){
+        int maxwater=0;
+          for(int i=0;i<height.size();i++){
+            for(int j=i+1;j<height.size();j++){
+                int ht=Math.min(height.get(i), height.get(j));
+                int width=j-i;
+                int currwater=ht*width;
+                maxwater=Math.max(maxwater, currwater);
+            }
+          }
+          return maxwater;
+    }
+    
+    public static void main(String[] args) {
+        ArrayList<Integer> height=new ArrayList<>();
+        height.add(4);
+        height.add(8);
+        height.add(6);
+        height.add(2);
+        height.add(5);
+        height.add(4);
+        height.add(8);
+        height.add(3);
+        height.add(7);
+        
+        System.out.println(storewater(height));
+    }
+    
+}
