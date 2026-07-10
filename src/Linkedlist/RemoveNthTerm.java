ddfbafed0d3cfcc0ab8599c5159cf29caa13f98a
Gagan Prajapati <gaganprajapati121@gmail.com>
2026-07-10 18:32:05 +0530

    Initial commit

--- src/Linkedlist/RemoveNthTerm.java
+++ src/Linkedlist/RemoveNthTerm.java
@@ -32,6 +32,28 @@ public class RemoveNthTerm {
         }
         System.out.println("null");
     }
+
+    public void deleteNthTerm(int n){
+        int sz=0;
+        Node temp=head;
+        while(temp!=null){
+            sz++;
+            temp=temp.next;
+        }
+        if(n==sz){
+            head=head.next;
+            return;
+        }
+        int i=0;
+        int iToFind=sz-n;
+        Node prev=head;
+        while(i<iToFind-1){
+            prev=prev.next;
+            i++;
+        }
+        prev.next=prev.next.next;
+        return;
+    }
     public static void main(String[] args) {
         RemoveNthTerm ll=new RemoveNthTerm();
         ll.addlast(0);
@@ -41,7 +63,10 @@ public class RemoveNthTerm {
         ll.addlast(4);
         ll.addlast(5);
         ll.printList();
-        System.out.println(size);
+        System.out.println("Size : "+size);
+        ll.deleteNthTerm(2);
+         ll.printList();
+        System.out.println("Size : "+size);
     }
     
 }
