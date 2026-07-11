0d402d5c5737f7a08999294c06bd54048a86b4c3
Gagan Prajapati <gaganprajapati121@gmail.com>
2026-07-12 02:12:15 +0530

    Initial commit

---
+++ src/Linkedlist/RemoveCycleLL.java
@@ -0,0 +1,73 @@
+package Linkedlist;
+
+public class RemoveCycleLL {
+    public static class Node{
+        Node next;
+        int data;
+        public Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    //detect Cycle..
+    public static boolean Iscycle(Node head){
+        Node slow=head;
+        Node fast=head;
+        while(fast!=null&&fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void removecycle(Node head){
+        //detect Cycle..
+        Node slow=head;
+        Node fast=head;
+        boolean cycle=false;
+        while(fast!=null&&fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
+                cycle=true;
+                break;
+            }
+        }
+        if(cycle==false){
+            return;
+        }
+        //find meeting point..
+        slow =head;
+        while(slow!=fast){
+            slow=slow.next;
+            fast=fast.next;
+        }
+        // Now slow = cycle start
+        // Find last node
+            Node last = slow;
+            while (last.next != slow) {
+                last = last.next;
+            }
+
+        //remove cycle->last.next=null..
+         last.next = null;
+    }
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.next=new Node(2);
+        head.next.next=new Node(3);
+        head.next.next.next=head;
+        System.out.println(Iscycle(head));
+        removecycle(head);
+        System.out.println(Iscycle(head));
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+    
+}
