0377021a07740dd2514364b45fec606c01e83f85
Gagan Prajapati <gaganprajapati121@gmail.com>
2026-07-11 23:58:51 +0530

    Initial commit

---
+++ src/Linkedlist/DeleteDuplicates.java
@@ -0,0 +1,58 @@
+package Linkedlist;
+
+public class DeleteDuplicates {
+    public static class Node{
+        int data;
+        Node next;
+        private boolean val;
+        public Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public static Node head;
+    public Node tail;
+
+    public void addlast(int data){
+        Node newNode=new Node(data);
+        if(head==null){
+            head=tail=newNode;
+            return;
+        }
+        tail.next=newNode;
+        tail=newNode;
+    }
+    public void printList(){
+        Node temp=head;
+        while(temp!=null){
+            System.out.print(temp.data+"->");
+            temp=temp.next;
+        }
+        System.out.println("null");
+    }
+    public Node deleteFunction(Node head){
+        Node curr=head;
+        while(curr!=null&&curr.next!=null){
+            if(curr.val==curr.next.val){
+                curr.next=curr.next.next;
+            }
+            else{
+                curr=curr.next;
+            }
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        DeleteDuplicates ll=new DeleteDuplicates();
+        ll.addlast(0);
+        ll.addlast(0);
+        ll.addlast(1);
+        ll.addlast(1);
+        ll.addlast(2);
+        ll.addlast(3);
+        ll.printList();
+        System.out.println(ll.deleteFunction(head));
+        ll.printList();
+    }
+}
