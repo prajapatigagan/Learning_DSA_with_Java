0746f146638700f0e7320773a257d16fda80f61c
Gagan Prajapati <gaganprajapati121@gmail.com>
2026-07-13 18:16:45 +0530

    Initial commit

---
+++ src/Stack/UsingArraylistOP.java
@@ -0,0 +1,43 @@
+package Stack;
+
+import java.util.ArrayList;
+
+public class UsingArraylistOP {
+    public static class stack{
+        static ArrayList<Integer> list=new ArrayList<>();
+        public static boolean isEmpty(){
+            return list.size()==0;
+        }
+        //push
+        public static void push(int data){
+            list.add(data);
+        }
+        //pop
+        public static int pop(){
+            if(isEmpty()){
+                return -1;
+            }
+            int top=list.get(list.size()-1);
+            list.remove(list.size()-1);
+            return top;
+        }
+        //peek
+        public static int peek(){
+            if(isEmpty()){
+                return -1;
+            }
+            return list.get(list.size()-1);
+        }
+    }
+    public static void main(String[] args) {
+        stack s=new stack();
+        s.push(0);
+        s.push(1);
+        s.push(3);
+        s.push(4);
+        while(!s.isEmpty()){
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
+}
