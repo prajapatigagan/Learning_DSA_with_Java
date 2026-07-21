f0b01200dff5e65018e92915413afe6d202c54c9
Gagan Prajapati <gaganprajapati121@gmail.com>
2026-07-21 18:08:15 +0530

    Initial commit

---
+++ src/GreedyAlgo/ActivitySection.java
@@ -0,0 +1,71 @@
+package GreedyAlgo;
+
+import java.util.*;
+
+public class ActivitySection {
+    //Allready sorting End program
+    // public static void main(String[] args) {
+    //     int start[]={1,3,0,5,8,5};
+    //     int end[]={2,4,6,7,9,9};
+
+
+        
+    //     int maxAct=0;
+    //     ArrayList<Integer> ans=new ArrayList<>();
+
+    //     //1st activity
+    //     maxAct=1;
+    //     ans.add(0);
+    //     int lastEnd=end[0];
+    //     for(int i=1;i<end.length;i++){
+    //         if(start[i]>=lastEnd){
+    //             //activity select
+    //             maxAct++;
+    //             ans.add(i);
+    //             lastEnd=end[i];
+    //         }
+    //     }
+    //     System.out.println("max activity="+maxAct);
+    //     for(int i=0; i<ans.size();i++){
+    //         System.out.print("A"+ans.get(i)+" ");
+    //     }
+    //     System.out.println();
+    // }
+
+    //nonsorting End program 
+        public static void main(String[] args) {
+        int start[]={1,3,0,5,8,5};
+        int end[]={2,4,6,7,9,9};
+
+        //sorting
+        int activities[][]=new int[start.length][3];
+        for(int i=0;i<start.length;i++){
+            activities[i][0]=i;
+            activities[i][1]=start[i];
+            activities[i][2]=end[i];
+        }
+        //lambda function -> shortform
+        Arrays.sort(activities,Comparator.comparingDouble(o -> o[2]));
+        
+        int maxAct=0;
+        ArrayList<Integer> ans=new ArrayList<>();
+
+        //1st activity
+        maxAct=1;
+        ans.add(activities[0][0]);
+        int lastEnd=activities[0][2];
+        for(int i=1;i<end.length;i++){
+            if(activities[i][1]>=lastEnd){
+                //activity select
+                maxAct++;
+                ans.add(activities[i][0]);
+                lastEnd=activities[i][2];
+            }
+        }
+        System.out.println("max activity="+maxAct);
+        for(int i=0; i<ans.size();i++){
+            System.out.print("A"+ans.get(i)+" ");
+        }
+        System.out.println();
+    }
+}
