2b1e14c7b38ca0b9599178ba3115498cd6f24287
Gagan Prajapati <gaganprajapati121@gmail.com>
2026-07-31 23:11:10 +0530

    Initial commit

--- src/Trees/BinarySearchTree/BuildBST.java
+++ src/Trees/BinarySearchTree/BuildBST.java
@@ -1,7 +1,43 @@
 package Trees.BinarySearchTree;
 
 public class BuildBST {
+    public static class Node{
+        int data;
+        Node left;
+        Node right;
+        public Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+    public static Node Insert(Node root,int val){
+        if(root==null){
+            root=new Node(val);
+            return root;
+        }
+        if(root.data>val){
+            root.left=Insert(root.left, val);
+        }
+        else{
+            root.right=Insert(root.right, val);
+        }
+        return root;
+    }
+    public static void Inorder(Node root){
+        if(root==null){
+            return;
+        }
+        Inorder(root.left);
+        System.out.print(root.data+" ");
+        Inorder(root.right);
+    }
     public static void main(String[] args) {
-        
+        int values[]={5,1,3,4,2,7};
+        Node root=null;
+        for(int i=0;i<values.length;i++){
+            root=Insert(root,values[i]);
+        }
+        Inorder(root);
     }
 }
